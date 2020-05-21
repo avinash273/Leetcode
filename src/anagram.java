2049b815ea6f207d217455ae27b7e21de71f55e3
avinash273 <avinash.shanker@mavs.uta.edu>
2020-05-21 01:53:42 -0500

    anagrams

--- src/anagram.java
+++ src/anagram.java
@@ -4,14 +4,20 @@ public class anagram {
     public ArrayList<ArrayList<String>> groupAnagrams(String[] strs) {
         if (strs.length == 0) return new ArrayList();
         HashMap<String, List> ans = new HashMap<String, List>();
+        System.out.println("strs:" + Arrays.toString(strs));
         for (String s : strs) {
+            System.out.println("s:" + s);
             char[] ca = s.toCharArray();
+            System.out.println("ca before:" + Arrays.toString(ca));
             Arrays.sort(ca);
+            System.out.println("ca after:" + Arrays.toString(ca));
             String key = String.valueOf(ca);
+            System.out.println("key:" + key);
             if (!ans.containsKey(key)) ans.put(key, new ArrayList());
             ans.get(key).add(s);
         }
-        System.out.println(new ArrayList(ans.values()));
+        System.out.println("ans:" + ans);
+        System.out.println("new ArrayList(ans.values())" + new ArrayList(ans.values()));
         return new ArrayList(ans.values());
     }
 }
