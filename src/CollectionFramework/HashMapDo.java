a361c1298367959e767f5158cbf6a6d73781c11c
avinash273 <avinash.shanker@mavs.uta.edu>
2020-05-25 03:41:32 -0500

    HashMap for Object of Book type

--- src/CollectionFramework/HashMapDo.java
+++ src/CollectionFramework/HashMapDo.java
@@ -17,19 +17,50 @@ public class HashMapDo {
         //Combine 2 HashMaps
         map.putAll(map1);
 
-        System.out.println("map.keySet(): "+map.keySet());
+        System.out.println("map.keySet(): " + map.keySet());
         //pass key get value
-        System.out.println("map.get(103): "+map.get(103));
+        System.out.println("map.get(103): " + map.get(103));
 
         //iterating on HashMap
         System.out.println("\n");
-        for(Map.Entry m: map.entrySet()){
-            System.out.println(m.getKey()+" "+m.getValue());
+        for (Map.Entry m : map.entrySet()) {
+            System.out.println(m.getKey() + " " + m.getValue());
         }
-
-
         System.out.println(map);
 
+        /**
+         * Trying to implement it for class Book, note there are differences in iterating over values
+         * so its, important, so check okay
+         */
+        class Book {
+            int ID;
+            String Name;
+            int cost;
+
+            Book(int ID, String Name, int cost) {
+                this.ID = ID;
+                this.Name = Name;
+                this.cost = cost;
+            }
+        }
+        HashMap<Integer, Book> BookMap = new HashMap<Integer, Book>();
+        Book b1 = new Book(1, "A", 10);
+        Book b2 = new Book(2, "B", 11);
+        Book b3 = new Book(3, "C", 12);
+        Book b4 = new Book(4, "D", 13);
+
+        BookMap.put(101, b1);
+        BookMap.put(102, b2);
+        BookMap.put(103, b3);
+        BookMap.put(104, b4);
+
+        //Now trying the iterator for hash of a class type
+        for (Map.Entry<Integer, Book> entry : BookMap.entrySet()) {
+            int key = entry.getKey();
+            Book b = entry.getValue();
+
+            System.out.println("Key: " + key + " ID: " + b.ID);
+        }
 
     }
 }
