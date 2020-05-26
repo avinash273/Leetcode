b28d15594c002b8b18b75b95ffeec5dd032eb9a8
avinash273 <avinash.shanker@mavs.uta.edu>
2020-05-26 00:26:04 -0500

    has-a address, employee

--- src/Basics/inheritanceTry.java
+++ src/Basics/inheritanceTry.java
@@ -26,11 +26,47 @@ class cat extends Animal {
  * Now trying out Aggregation Has-a
  */
 
+class address{
+    String city, state, country;
+
+    address(String city,String state,String country){
+        this.city = city;
+        this.state = state;
+        this.country = country;
+    }
+}
+
+class employee{
+    int ID;
+    String name;
+    address homeAddress;
+
+    employee(int ID, String name, address homeAddress){
+        this.ID = ID;
+        this.name = name;
+        this.homeAddress = homeAddress;
+    }
+
+    void display(){
+        System.out.println(this.ID+" "+this.name);
+        System.out.println(this.homeAddress.city+" "+this.homeAddress.state+" "+this.homeAddress.country);
+    }
+}
+
 
 public class inheritanceTry {
     void implementAnimals() {
         cat cat = new cat();
         cat.meaow();
         cat.eat();
+
+        address address1 = new address("Bangalore","Karnataka","India");
+        address address2 = new address("Kolkata","West Bengal","India");
+
+
+        employee emp1 = new employee(100,"avinash",address1);
+        employee emp2 = new employee(101,"akash",address2);
+        emp1.display();
+        emp2.display();
     }
 }
