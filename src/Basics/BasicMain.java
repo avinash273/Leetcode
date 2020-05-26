08c9b21e61e63397f0a1d0d78d1af326e98727c6
avinash273 <avinash.shanker@mavs.uta.edu>
2020-05-25 19:51:11 -0500

    switch simple

---
+++ src/Basics/BasicMain.java
@@ -0,0 +1,9 @@
+package Basics;
+
+public class BasicMain {
+    public static void main(String[] args) {
+        //Simple switch example
+        SwitchDo SwitchDo = new SwitchDo();
+        SwitchDo.implementSwitch();
+    }
+}
---
+++ src/Basics/SwitchDo.java
@@ -0,0 +1,18 @@
+package Basics;
+
+public class SwitchDo {
+    void implementSwitch() {
+        int number = 20;
+        switch (number) {
+            case 10:
+                System.out.println("10");
+                break;
+            case 20:
+                System.out.println("20");
+                break;
+            default:
+                System.out.println("None");
+                break;
+        }
+    }
+}
