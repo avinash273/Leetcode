17010943915c5a3b966113c697a51929df8e45d6
avinash273 <avinash.shanker@mavs.uta.edu>
2020-05-27 14:52:24 -0500

    updated to Java11

--- src/Leetcode/Helper.java
+++ src/Leetcode/Helper.java
@@ -3,6 +3,16 @@ package Leetcode;
 public class Helper extends print {
 
     public static void main(String[] args) {
+        /**
+         * Was updating from Java8 to Java11, hence for version check.
+         * System.out.println(System.getProperty("java.version"));
+         * System.out.println(System.getProperty("java.runtime.version"));
+         * System.out.println(System.getProperty("java.home"));
+         * System.out.println(System.getProperty("java.vendor"));
+         * System.out.println(System.getProperty("java.vendor.url"));
+         * System.out.println(System.getProperty("java.class.path"));
+         */
+
         System.out.println("Leetcode.Helper function to call all functionalities");
         /*
         //To get GCD
@@ -38,6 +48,6 @@ public class Helper extends print {
 
         //save boats
         saveBoats saveBoats = new saveBoats();
-        print(saveBoats.saveBoats(new int[]{1,1,2,2,2,2,2,3}, 3));
+        print(saveBoats.saveBoats(new int[]{1, 1, 2, 2, 2, 2, 2, 3}, 3));
     }
 }
