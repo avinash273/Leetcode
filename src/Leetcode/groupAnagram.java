891c3adfdd2b0eb990ca878ca9d3a1ce508f7233
avinash273 <avinash.shanker@mavs.uta.edu>
2020-05-30 20:36:50 -0500

    groupAnagram

---
+++ src/Leetcode/groupAnagram.java
@@ -0,0 +1,19 @@
+package Leetcode;
+import java.util.*;
+
+class groupAnagram {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List> map = new HashMap<String, List>();
+
+        for (String each: strs){
+            char[] charStr = each.toCharArray();
+            Arrays.sort(charStr);
+            String key = Arrays.toString(charStr);
+
+            if(!map.containsKey(key))
+                map.put(key, new ArrayList());
+            map.get(key).add(each);
+        }
+        return new ArrayList(map.values());
+    }
+}
