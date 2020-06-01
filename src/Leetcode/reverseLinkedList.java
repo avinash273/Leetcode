9bbb0494ea69e30b3d2dc9a7b5d0d03b0453bece
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-01 02:14:48 -0500

    reverse linked list

---
+++ src/Leetcode/reverseLinkedList.java
@@ -0,0 +1,33 @@
+package Leetcode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class ListNode {
+ int val;
+ ListNode next;
+ ListNode() {}
+ ListNode(int val) { this.val = val; }
+ ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+class reverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
