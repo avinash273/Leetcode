c761a52120ba719fab378a31ded9c33e9f2265d5
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-05 18:36:07 -0500

    binary Tree Inorder

---
+++ src/Leetcode/binaryTreeInorder.java
@@ -0,0 +1,27 @@
+package Leetcode;
+
+import java.util.*;
+
+class binaryTreeInorder {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        List<Integer> outputArr = new ArrayList<>();
+
+        if(root == null){
+            return outputArr;
+        }
+
+        TreeNode current = root;
+
+        while(current != null || !stack.isEmpty()){
+            while(current != null){
+                stack.push(current);
+                current = current.left;
+            }
+            current = stack.pop();
+            outputArr.add(current.val);
+            current = current.right;
+        }
+        return outputArr;
+    }
+}
