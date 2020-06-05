9e7ed68ea14464d1106077a0a80be8b4f67a0313
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-05 15:56:55 -0500

    palindrome Linked List

---
+++ src/Leetcode/palindromeLinkedList.java
@@ -0,0 +1,37 @@
+package Leetcode;
+
+class palindromeLinkedList {
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        slow = reverse(slow);
+        fast = head;
+
+        while (slow != null) {
+            if (slow.val != fast.val) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return true;
+    }
+
+}
