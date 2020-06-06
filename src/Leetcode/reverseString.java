220ae662b2e00e7e8fd0f61e5c7c43c9d3ed01d5
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-05 19:37:11 -0500

    reverse number

---
+++ src/Leetcode/reverseString.java
@@ -0,0 +1,31 @@
+package Leetcode;
+
+/**
+ * Time complexity : O(log10(n))
+ * Space complexity : O(1)
+ */
+public class reverseString {
+}
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x == 0)
+            return true;
+
+        if(x < 0 || x % 10 == 0)
+            return false;
+
+        int reversedInt = 0;
+        while(x > reversedInt){
+            int pop = x % 10;
+            x /= 10;
+
+            reversedInt = (reversedInt * 10) + pop;
+        }
+
+        if(x == reversedInt || x == reversedInt/10){
+            return true;
+        }
+        return false;
+    }
+}
