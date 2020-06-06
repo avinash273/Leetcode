ede2068244b0cfab06b9ebb564cf7c2a26c429cf
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-06 02:48:38 -0500

    ip Addrress

---
+++ src/Leetcode/ipAddrress.java
@@ -0,0 +1,19 @@
+package Leetcode;
+
+/**
+ * IP address defanging, uses string builder.
+ */
+
+class ipAddrress {
+    public String defangIPaddr(String address) {
+        StringBuilder s = new StringBuilder();
+
+        for(char c:address.toCharArray()){
+            if(c == '.')
+                s.append("[.]");
+            else
+                s.append(c);
+        }
+        return s.toString();
+    }
+}
