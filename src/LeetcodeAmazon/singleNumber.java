592ba478590a8b428de1aac6be35642a37d7185a
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-13 22:07:14 -0500

    valid parentheses complexity added

---
+++ src/LeetcodeAmazon/singleNumber.java
@@ -0,0 +1,28 @@
+package LeetcodeAmazon;
+
+import java.util.HashSet;
+
+public class singleNumber {
+}
+
+/**
+ * T: O(n)
+ * S: O(n)
+ */
+class Solution3 {
+    public int singleNumber(int[] nums) {
+        HashSet<Integer> set = new HashSet<Integer>();
+
+        for(Integer each: nums){
+            if(set.contains(each))
+                set.remove(each);
+            else
+                set.add(each);
+        }
+
+        for(int i: set)
+            return i;
+
+        return -1;
+    }
+}
