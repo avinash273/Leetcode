eec78d71449780b7d693c5fd97059273cca8c797
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-13 22:25:44 -0500

    fizz buzz

---
+++ src/LeetcodeAmazon/fizzBuzz.java
@@ -0,0 +1,30 @@
+package LeetcodeAmazon;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class fizzBuzz {
+}
+
+/**
+ * T: O(n)
+ * S: O(n)
+ */
+
+class Solution4 {
+    public List<String> fizzBuzz(int n) {
+        ArrayList<String> output = new  ArrayList<String>();
+
+        for(int i = 1; i <= n; i++){
+            if(i % 3 == 0 && i % 5 == 0)
+                output.add("FizzBuzz");
+            else if(i % 3 == 0)
+                output.add("Fizz");
+            else if(i % 5 == 0)
+                output.add("Buzz");
+            else
+                output.add(Integer.toString(i));
+        }
+        return output;
+    }
+}
