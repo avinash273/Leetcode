a573f33f1c8b953583bfc13f72a59ae28d2b2197
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-13 21:25:57 -0500

    valid parentheses

---
+++ src/LeetcodeAmazon/validParentheses.java
@@ -0,0 +1,33 @@
+package LeetcodeAmazon;
+
+import java.util.Stack;
+
+public class validParentheses {
+}
+
+class Solution1 {
+    public boolean isValid(String s) {
+
+        if(s == null || s.length() <= 0)
+            return true;
+
+        Stack<Character> stack = new Stack<Character>();
+
+        for(char each: s.toCharArray()){
+            if(each == '(' || each == '[' || each == '{')
+                stack.push(each);
+            else if(each == ')' && !stack.isEmpty() && stack.peek() == '(')
+                stack.pop();
+            else if(each == ']' && !stack.isEmpty() && stack.peek() == '[')
+                stack.pop();
+            else if(each == '}' && !stack.isEmpty() && stack.peek() == '{')
+                stack.pop();
+            else
+                return false;
+
+        }
+
+        return stack.isEmpty();
+
+    }
+}
