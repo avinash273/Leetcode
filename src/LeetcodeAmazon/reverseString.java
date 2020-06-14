ea46b683f820a2486817f179cd142622f16e9d0d
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-14 12:38:32 -0500

    reverse String

---
+++ src/LeetcodeAmazon/reverseString.java
@@ -0,0 +1,24 @@
+package LeetcodeAmazon;
+
+/***
+ * T: O(n)
+ * S: O(1)
+ */
+
+public class reverseString {
+    class Solution {
+        public void reverseString(char[] s) {
+
+            int i = 0;
+            int j = s.length-1;
+            char temp;
+            while(i <= j){
+                temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+                i++;
+                j--;
+            }
+        }
+    }
+}
