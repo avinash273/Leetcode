ed4af32943b16398676302822ca5e4f1796c9c7b
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-14 20:12:48 -0500

    plus One Array

---
+++ src/LeetcodeAmazon/plusOneArray.java
@@ -0,0 +1,24 @@
+package LeetcodeAmazon;
+
+/**
+ * T: O(n)
+ * S: O(1)
+ */
+public class plusOneArray {
+    class Solution {
+        public int[] plusOne(int[] digits) {
+
+            for(int i = digits.length - 1; i >= 0; i--){
+                if(digits[i] < 9){
+                    digits[i]++;
+                    return digits;
+                }
+                digits[i] = 0;
+
+            }
+            int [] results = new int[digits.length + 1];
+            results[0] = 1;
+            return results;
+        }
+    }
+}
