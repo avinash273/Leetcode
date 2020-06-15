1a61e6cf49d83eca61730be04a483aacaee35ae1
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-14 19:44:30 -0500

    reverse integer

---
+++ src/LeetcodeAmazon/reverseInteger.java
@@ -0,0 +1,29 @@
+package LeetcodeAmazon;
+
+/**
+ * T: O(log10 n)
+ * S: O(1)
+ */
+public class reverseInteger {
+    class Solution {
+        public int reverse(int x) {
+            boolean negative = false;
+
+            if (x < 0) {
+                negative = true;
+                x *= -1;
+            }
+
+            long reversed = 0;
+            while (x > 0) {
+                reversed = (reversed * 10) + (x % 10);
+                x /= 10;
+            }
+
+            if (reversed > Integer.MAX_VALUE)
+                return 0;
+
+            return negative ? (int) reversed * (-1) : (int) reversed;
+        }
+    }
+}
