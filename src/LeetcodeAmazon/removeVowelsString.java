001ed77058c3544ed94dd9d1c5e00d847309e98e
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-16 02:19:12 -0500

    remove vovels from string

---
+++ src/LeetcodeAmazon/removeVowelsString.java
@@ -0,0 +1,28 @@
+package LeetcodeAmazon;
+
+/**
+ * T: O(n)
+ * S: O(1)
+ */
+
+public class removeVowelsString {
+    class Solution {
+        public String removeVowels(String S) {
+            int i = 0;
+            int size = 0;
+            for(char c: S.toCharArray()){
+                if( c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+                    size++;
+                }
+            }
+            char[] output = new char[S.length() - size];
+            for(char c: S.toCharArray()){
+                if( c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u'){
+                    output[i] = c;
+                    i++;
+                }
+            }
+            return new String(output);
+        }
+    }
+}
