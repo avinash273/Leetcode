fb49e85a14338a50b4d89637a2b0d7b98f21fd3f
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-16 01:31:57 -0500

    haming distane

---
+++ src/LeetcodeAmazon/hammingDistance.java
@@ -0,0 +1,20 @@
+package LeetcodeAmazon;
+
+/**
+ * T: O(1) as integer value is under a constant
+ * S: O(1)
+ */
+
+public class hammingDistance {
+    class Solution {
+        public int hammingDistance(int x, int y) {
+            int result = 0;
+            while(x > 0 || y > 0){
+                result += (x % 2) ^ (y % 2);
+                x >>= 1;
+                y >>= 1;
+            }
+            return result;
+        }
+    }
+}
