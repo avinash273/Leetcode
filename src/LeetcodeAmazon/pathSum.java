4ddf1503bad18f2bb749d9a719d807d21212f83a
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-16 01:57:09 -0500

    path sum and single number II

---
+++ src/LeetcodeAmazon/pathSum.java
@@ -0,0 +1,33 @@
+package LeetcodeAmazon;
+
+/**
+ * T: O(n)
+ * S: O(n)
+ */
+public class pathSum {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class Solution {
+        public boolean hasPathSum(TreeNode root, int sum) {
+            if(root == null)
+                return false;
+            else if(root.left == null && root.right == null && sum - root.val == 0)
+                return true;
+            else
+                return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+        }
+    }
+}
