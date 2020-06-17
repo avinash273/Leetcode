992c7382e952494eeb8b47434f9d5a69c2924b99
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-16 22:37:34 -0500

    lowest common ancestor of a BST

---
+++ src/LeetcodeAmazon/lowestCommonBST.java
@@ -0,0 +1,34 @@
+package LeetcodeAmazon;
+
+/**
+ * T: O(n)
+ * S: O(n)
+ */
+
+public class lowestCommonBST {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    class Solution {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if (root == p || root == q || root == null)
+                return root;
+            TreeNode left = lowestCommonAncestor(root.left, p, q);
+            TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+            if (left == null)
+                return right;
+            else if (right == null)
+                return left;
+            else
+                return root;
+        }
+    }
+}
