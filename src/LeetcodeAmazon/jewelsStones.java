d9e163e2c18a58f61109524bc0ef8f6878f6921b
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-16 23:14:42 -0500

    jewels and stones

---
+++ src/LeetcodeAmazon/jewelsStones.java
@@ -0,0 +1,24 @@
+package LeetcodeAmazon;
+
+import java.util.*;
+
+public class jewelsStones {
+    class Solution {
+        public int numJewelsInStones(String J, String S) {
+            Set<Character> set = new HashSet<Character>();
+            int output = 0;
+            if(J.length() == 0 || S.length() == 0)
+                return 0;
+
+            for(char each:J.toCharArray()){
+                set.add(each);
+            }
+
+            for(char each:S.toCharArray()){
+                if(set.contains(each))
+                    output++;
+            }
+            return output;
+        }
+    }
+}
