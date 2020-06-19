e5b3f034f71ef29894aaeb15638a21f0d530f4f9
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-18 20:39:29 -0500

    Trees

---
+++ src/LeetcodeTrees/BinaryPreTraversal.java
@@ -0,0 +1,22 @@
+package LeetcodeTrees;
+
+public class BinaryPreTraversal {
+    public class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(){}
+
+        TreeNode(int val){
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right){
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<Integer> preOrderTre
+}
