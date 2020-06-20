fa6c81cbd3c023b11a652ab89b659eb3185d5395
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-19 19:24:42 -0500

    count battle ships

--- src/LeetcodeAmazon/battleShipCount.java
+++ src/LeetcodeAmazon/battleShipCount.java
@@ -1,8 +1,13 @@
 package LeetcodeAmazon;
 
 /**
+ * sol1
  * T: O(n)
  * S: O(n)
+ *
+ * sol2
+ * T: O(n)
+ * S: O(1)
  */
 
 public class battleShipCount {
@@ -32,6 +37,24 @@ public class battleShipCount {
 
             return 1;
 
+        }
+    }
+
+    class Solution2 {
+        public int countBattleships(char[][] board) {
+            int numBattleShip = 0;
+            for(int i = 0; i < board.length; i++){
+                for(int j = 0; j < board[i].length; j++){
+                    if(board[i][j] == '.')
+                        continue;
+                    if(i > 0 && board[i - 1][j] == 'X')
+                        continue;
+                    if(j > 0 && board[i][j - 1] == 'X')
+                        continue;
+                    numBattleShip++;
+                }
+            }
+            return numBattleShip;
         }
     }
 
