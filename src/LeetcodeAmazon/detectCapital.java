572540c843b6c248a1be9d11fa4491b027a3f66a
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-22 01:08:37 -0500

    detect capital, more professional https://leetcode.com/problems/detect-capital/submissions/

--- src/LeetcodeAmazon/detectCapital.java
+++ src/LeetcodeAmazon/detectCapital.java
@@ -10,25 +10,25 @@ public class detectCapital {
     class Solution {
         public boolean detectCapitalUse(String word) {
             int len = word.length();
-            if(len == 0 || word == null)
+            if (len == 0 || word == null)
                 return true;
 
             int allCap = 0;
             int firstCap = 0;
             int noCap = 0;
 
-            for(int i = 0; i < len; i++){
-                if(Character.isUpperCase(word.charAt(i)))
+            for (int i = 0; i < len; i++) {
+                if (Character.isUpperCase(word.charAt(i)))
                     allCap++;
 
-                if(!Character.isUpperCase(word.charAt(i)))
+                if (!Character.isUpperCase(word.charAt(i)))
                     noCap++;
 
-                if(Character.isUpperCase(word.charAt(0)) && !Character.isUpperCase(word.charAt(i)) && i != 0)
+                if (Character.isUpperCase(word.charAt(0)) && !Character.isUpperCase(word.charAt(i)) && i != 0)
                     firstCap++;
             }
-            System.out.println("allCap:"+allCap+" noCap:"+noCap+" firstCap:"+firstCap+" len"+len);
-            if(allCap == len || noCap == len || firstCap == len-1)
+            System.out.println("allCap:" + allCap + " noCap:" + noCap + " firstCap:" + firstCap + " len" + len);
+            if (allCap == len || noCap == len || firstCap == len - 1)
                 return true;
             return false;
         }
@@ -44,18 +44,18 @@ public class detectCapital {
             int firstCap = 0;
             int noCap = 0;
 
-            for(int i = 0; i < len; i++){
-                if(Character.isUpperCase(word.charAt(i)))
+            for (int i = 0; i < len; i++) {
+                if (Character.isUpperCase(word.charAt(i)))
                     allCap++;
 
-                if(!Character.isUpperCase(word.charAt(i)))
+                if (!Character.isUpperCase(word.charAt(i)))
                     noCap++;
 
-                if(Character.isUpperCase(word.charAt(0)) && !Character.isUpperCase(word.charAt(i)) && i != 0)
+                if (Character.isUpperCase(word.charAt(0)) && !Character.isUpperCase(word.charAt(i)) && i != 0)
                     firstCap++;
             }
 
-            return (allCap == len || noCap == len || firstCap == len-1);
+            return (allCap == len || noCap == len || firstCap == len - 1);
         }
     }
 }
