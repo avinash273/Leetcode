6bb8adb0d1d5456c496f3d5095fc54055c504143
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-22 01:03:58 -0500

    group Anagrams complexity

---
+++ src/LeetcodeAmazon/detectCapital.java
@@ -0,0 +1,35 @@
+package LeetcodeAmazon;
+
+/**
+ * T: O(n) where n = letter count
+ * S: (1)
+ */
+
+public class detectCapital {
+    class Solution {
+        public boolean detectCapitalUse(String word) {
+            int len = word.length();
+            if(len == 0 || word == null)
+                return true;
+
+            int allCap = 0;
+            int firstCap = 0;
+            int noCap = 0;
+
+            for(int i = 0; i < len; i++){
+                if(Character.isUpperCase(word.charAt(i)))
+                    allCap++;
+
+                if(!Character.isUpperCase(word.charAt(i)))
+                    noCap++;
+
+                if(Character.isUpperCase(word.charAt(0)) && !Character.isUpperCase(word.charAt(i)) && i != 0)
+                    firstCap++;
+            }
+            System.out.println("allCap:"+allCap+" noCap:"+noCap+" firstCap:"+firstCap+" len"+len);
+            if(allCap == len || noCap == len || firstCap == len-1)
+                return true;
+            return false;
+        }
+    }
+}
