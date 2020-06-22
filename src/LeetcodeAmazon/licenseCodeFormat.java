5f16674725d2edd0585af473f51f491b9bb30941
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-21 23:58:48 -0500

    license code format

---
+++ src/LeetcodeAmazon/licenseCodeFormat.java
@@ -0,0 +1,35 @@
+package LeetcodeAmazon;
+
+/**
+ * T: O(n)
+ * S: O(1)
+ */
+public class licenseCodeFormat {
+
+    class AviSolution {
+        public String licenseKeyFormatting(String S, int K) {
+            int len = S.length();
+            if(len == 0 || S == null)
+                return S;
+
+            StringBuilder sb = new StringBuilder();
+
+            char[] letters = S.toCharArray();
+            int kSet = K;
+            for(int i = len - 1; i >= 0; i--){
+                if(letters[i] != '-'){
+                    sb.insert(0, Character.toUpperCase(letters[i]));
+                    if(kSet > 0)
+                        kSet--;
+                    if(kSet == 0){
+                        sb.insert(0, '-');
+                        kSet = K;
+                    }
+                }
+            }
+            if(sb.length() !=0 && sb.charAt(0) == '-')
+                sb.deleteCharAt(0);
+            return sb.toString();
+        }
+    }
+}
