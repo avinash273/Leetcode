d8a0a0a249f2f9be00e162fbb7a8428a5979ca54
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-21 20:32:46 -0500

    k closet to origin

---
+++ src/LeetcodeAmazon/kClosestToOrigin.java
@@ -0,0 +1,29 @@
+package LeetcodeAmazon;
+
+import java.util.PriorityQueue;
+
+/**
+ * T: O(n)
+ * S: O(k)
+ */
+
+public class kClosestToOrigin {
+    class Solution {
+        public int[][] kClosest(int[][] points, int K) {
+            PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> (b[0]*b[0] + b[1]*b[1] - (a[0]*a[0] + a[1]*a[1])));
+
+            for(int[] point: points){
+                maxHeap.add(point);
+                if(maxHeap.size() > K)
+                    maxHeap.remove();
+            }
+
+            int[][] result = new int[K][2];
+
+            while(K-- > 0){
+                result[K] = maxHeap.remove();
+            }
+            return result;
+        }
+    }
+}
