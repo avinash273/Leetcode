f3cf048e6d3d931387fdcf29ba721b37fa326f42
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-22 01:39:45 -0500

    path sumII

---
+++ src/LeetcodeAmazon/pathSumII.java
@@ -0,0 +1,48 @@
+package LeetcodeAmazon;
+
+import java.util.*;
+
+public class pathSumII {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        public List<List<Integer>> pathSum(TreeNode root, int sum) {
+            List<List<Integer>> paths = new ArrayList<>();
+            findPaths(root, sum, new ArrayList<Integer>(), paths);
+            return paths;
+        }
+
+        public void findPaths(TreeNode root, int sum, List<Integer> current, List<List<Integer>> paths) {
+            if (root == null)
+                return;
+
+            current.add(root.val);
+            if (root.val == sum && root.left == null && root.right == null) {
+                paths.add(current);
+                return;
+            }
+
+            findPaths(root.left, sum - root.val, new ArrayList<Integer>(current), paths);
+
+            findPaths(root.right, sum - root.val, new ArrayList<Integer>(current), paths);
+        }
+    }
+}
