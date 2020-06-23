a99246845910c0fa158ae9197cce8580514580a5
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-23 13:12:11 -0500

    sort Array By Parity O(n) O(1) solution

--- src/LeetcodeAmazon/sortArrayByParity.java
+++ src/LeetcodeAmazon/sortArrayByParity.java
@@ -1,6 +1,34 @@
 package LeetcodeAmazon;
 
 public class sortArrayByParity {
+
+    /**
+     * Best solution, need to revisit again
+     * T: O(n)
+     * S: O(1)
+     */
+    class Solution {
+        public int[] sortArrayByParity(int[] A) {
+            int index = 0;
+            for(int i = 0; i < A.length; i++){
+                int temp = 0;
+                if(A[i] % 2 == 0){
+                    temp = A[index];
+                    A[index++] = A[i];
+                    A[i] = temp;
+                }
+            }
+            return A;
+        }
+    }
+
+
+    /**
+     * Avi's first solution
+     * T: O(n)
+     * S: O(n)
+     * this should be done oin O(1) space
+     */
     class aviSolution {
         public int[] sortArrayByParity(int[] A) {
             int[] output = new int[A.length];
