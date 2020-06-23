c0524d1cc2e32548b8c12b6fb0845b71d1041210
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-23 10:56:20 -0500

    walls And Gates

---
+++ src/LeetcodeDFS/wallsAndGates.java
@@ -0,0 +1,31 @@
+package LeetcodeDFS;
+
+/**
+ * T: O(m x n)
+ * S: O(m x n)
+ */
+
+public class wallsAndGates {
+    class Solution {
+        public void wallsAndGates(int[][] rooms) {
+            for(int i = 0; i < rooms.length; i++){
+                for(int j = 0; j < rooms[i].length; j++){
+                    if(rooms[i][j] == 0)
+                        dfs(i, j, 0, rooms);
+                }
+            }
+        }
+
+        private void dfs(int i, int j, int count, int[][] rooms){
+            if(i < 0 || i >= rooms.length || j < 0 || j >= rooms[i].length || rooms[i][j] < count)
+                return;
+
+            rooms[i][j] = count;
+            dfs(i + 1, j, count + 1, rooms);
+            dfs(i - 1, j, count + 1, rooms);
+            dfs(i, j + 1, count + 1, rooms);
+            dfs(i, j - 1, count + 1, rooms);
+
+        }
+    }
+}
