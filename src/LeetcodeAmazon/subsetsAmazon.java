1d881d97e78e19f5b768f674ab3e57f75da45afd
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-24 00:25:03 -0500

    subsets amazon

---
+++ src/LeetcodeAmazon/subsetsAmazon.java
@@ -0,0 +1,29 @@
+package LeetcodeAmazon;
+
+import java.util.*;
+
+/**
+ * very important, method.
+ * Need to learn thoroughly
+ * T: O(2^n) every single me are making a choice
+ * S: O(n)
+ */
+public class subsetsAmazon {
+    class Solution {
+        public List<List<Integer>> subsets(int[] nums) {
+            List<List<Integer>> subsets = new ArrayList<>();
+            generateSubsets(0, nums, new ArrayList<Integer>(), subsets);
+            return subsets;
+        }
+
+        void generateSubsets(int index, int[] nums, List<Integer> current, List<List<Integer>> subsets) {
+            subsets.add(new ArrayList<>(current));
+
+            for (int i = index; i < nums.length; i++) {
+                current.add(nums[i]);
+                generateSubsets(i + 1, nums, current, subsets);
+                current.remove(current.size() - 1);
+            }
+        }
+    }
+}
