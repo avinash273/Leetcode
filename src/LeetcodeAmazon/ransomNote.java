2903a4b20ce361a4e62ade593a6633b4ee46c7e0
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-23 19:52:25 -0500

    ransom note

---
+++ src/LeetcodeAmazon/ransomNote.java
@@ -0,0 +1,23 @@
+package LeetcodeAmazon;
+
+import java.util.HashMap;
+
+public class ransomNote {
+    class Solution {
+        public boolean canConstruct(String ransomNote, String magazine) {
+            HashMap<Character, Integer> ransomMap = new HashMap<>();
+
+            for(char each: magazine.toCharArray())
+                ransomMap.put(each, ransomMap.getOrDefault(each, 0) + 1);
+
+            for(char each: ransomNote.toCharArray()){
+                if(ransomMap.containsKey(each) && ransomMap.get(each) >= 1){
+                    ransomMap.put(each, ransomMap.get(each) - 1);
+                }
+                else
+                    return false;
+            }
+            return true;
+        }
+    }
+}
