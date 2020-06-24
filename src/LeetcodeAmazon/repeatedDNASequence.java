d3c959e393d57081ccbb481d8c9ff419b9f130a1
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-24 15:50:43 -0500

    repeated DNA subsequence

--- src/LeetcodeAmazon/repeatedDNASequence.java
+++ src/LeetcodeAmazon/repeatedDNASequence.java
@@ -2,6 +2,11 @@ package LeetcodeAmazon;
 
 import java.util.*;
 
+/**
+ * revist again, good problem
+ * T: O(n)
+ * S: O(n)
+ */
 public class repeatedDNASequence {
     class Solution {
         public List<String> findRepeatedDnaSequences(String s) {
