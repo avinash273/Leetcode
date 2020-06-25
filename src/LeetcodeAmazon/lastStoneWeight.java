745aa558512bf2a615f44712dab19726e0f40800
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-24 21:28:43 -0500

    Last Stone Weight

---
+++ src/LeetcodeAmazon/lastStoneWeight.java
@@ -0,0 +1,30 @@
+package LeetcodeAmazon;
+
+import java.util.PriorityQueue;
+
+/**
+ * https://leetcode.com/problems/last-stone-weight/
+ * T: O(n log n) as its a priority Queue
+ * S: O(n)
+ */
+
+public class lastStoneWeight {
+    class Solution {
+        public int lastStoneWeight(int[] stones) {
+            PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b-a);
+
+            for(int stone: stones)
+                maxHeap.add(stone);
+
+            while(maxHeap.size() > 1){
+                int stoneOne = maxHeap.remove();
+                int stoneTwo = maxHeap.remove();
+
+                if(stoneOne != stoneTwo){
+                    maxHeap.add(stoneOne - stoneTwo);
+                }
+            }
+            return maxHeap.isEmpty() ? 0 : maxHeap.remove();
+        }
+    }
+}
