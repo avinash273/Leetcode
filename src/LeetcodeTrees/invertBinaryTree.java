1d416a0aa092417dc9547c9ffaffabfa5a5c6154
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-26 18:53:45 -0500

    invert binary tree

---
+++ src/LeetcodeTrees/invertBinaryTree.java
@@ -0,0 +1,43 @@
+package LeetcodeTrees;
+
+/**
+ * T: O(n)
+ * S: O(n)
+ */
+
+public class invertBinaryTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        public TreeNode invertTree(TreeNode root) {
+            if (root == null)
+                return root;
+
+            TreeNode right = invertTree(root.right);
+            TreeNode left = invertTree(root.left);
+
+            root.left = right;
+            root.right = left;
+
+            return root;
+
+        }
+    }
+}
