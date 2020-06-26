b4dfa322d93e916f120bac6719ca95922c1da593
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-26 15:46:13 -0500

    pascals Triangle

---
+++ src/LeetcodeAmazon/pascalsTriangle.java
@@ -0,0 +1,38 @@
+package LeetcodeAmazon;
+
+import java.util.*;
+
+/**
+ * T: O(n)
+ * S: O(n)
+ */
+
+public class pascalsTriangle {
+    class Solution {
+        public List<List<Integer>> generate(int numRows) {
+            List<List<Integer>> triangle = new ArrayList<>();
+
+            if(numRows == 0)
+                return triangle;
+
+            List<Integer> firstRow = new ArrayList<>();
+            firstRow.add(1);
+            triangle.add(firstRow);
+
+            for(int i = 1; i < numRows; i++){
+                List<Integer> prevRow = triangle.get(i - 1);
+                List<Integer> row = new ArrayList<>();
+
+                row.add(1);
+
+                for(int j = 1; j < i; j++){
+                    row.add(prevRow.get(j - 1) + prevRow.get(j));
+                }
+
+                row.add(1);
+                triangle.add(row);
+            }
+            return triangle;
+        }
+    }
+}
