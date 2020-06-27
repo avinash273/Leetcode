cc2e244e9d8716362e18be1e5f46e7478356199f
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-26 21:58:33 -0500

    keys And Rooms amazon

---
+++ src/LeetcodeAmazon/keysAndRooms.java
@@ -0,0 +1,39 @@
+package LeetcodeAmazon;
+
+import java.util.*;
+
+/**
+ * T: O(n)
+ * S: (n)
+ * https://leetcode.com/problems/keys-and-rooms/
+ */
+public class keysAndRooms {
+    class Solution {
+        public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+            if(rooms == null)
+                return false;
+
+            boolean[] unlocked = new boolean[rooms.size()];
+            unlocked[0] = true;
+
+            Stack<Integer> keys = new Stack<>();
+            keys.push(0);
+
+            while(!keys.isEmpty()){
+                for(int newKey : rooms.get(keys.pop())){
+                    if(!unlocked[newKey]){
+                        unlocked[newKey] = true;
+                        keys.push(newKey);
+                    }
+                }
+            }
+
+            for(int i = 0; i < unlocked.length; i++){
+                if(!unlocked[i])
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
