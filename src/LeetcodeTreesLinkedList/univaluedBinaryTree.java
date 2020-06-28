62b4a1d7eb5eaf73c141572c0dc8690db5d0e1c2
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-28 02:02:20 -0500

    uni-valued Binary Tree

---
+++ src/LeetcodeTreesLinkedList/univaluedBinaryTree.java
@@ -0,0 +1,33 @@
+package LeetcodeTreesLinkedList;
+
+public class univaluedBinaryTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        public boolean isUnivalTree(TreeNode root) {
+            boolean leftCheck = (root.left == null || (root.val == root.left.val && isUnivalTree(root.left)));
+
+            boolean rightCheck = (root.right == null || (root.val == root.right.val && isUnivalTree(root.right)));
+
+            return leftCheck && rightCheck;
+        }
+    }
+}
