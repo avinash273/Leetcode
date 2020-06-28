a59f1df9ddc469a6ffa55c854cd665c8b0387306
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-28 01:48:32 -0500

    partition list based on pivot

---
+++ src/LeetcodeTreesLinkedList/bottomLeftTreeValue.java
@@ -0,0 +1,41 @@
+package LeetcodeTreesLinkedList;
+
+import java.util.*;
+
+public class bottomLeftTreeValue {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        public int findBottomLeftValue(TreeNode root) {
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.add(root);
+
+            while (!queue.isEmpty()) {
+                root = queue.poll();
+                if (root.right != null)
+                    queue.add(root.right);
+                if (root.left != null)
+                    queue.add(root.left);
+            }
+            return root.val;
+        }
+    }
+}
---
+++ src/LeetcodeTreesLinkedList/partitionList.java
@@ -0,0 +1,49 @@
+package LeetcodeTreesLinkedList;
+
+/**
+ * T: O(n)
+ * S: O(1)
+ */
+
+public class partitionList {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    class Solution {
+        public ListNode partition(ListNode head, int x) {
+            ListNode beforeHead = new ListNode(-1);
+            ListNode before = beforeHead;
+            ListNode afterHead = new ListNode(-1);
+            ListNode after = afterHead;
+
+            while (head != null) {
+                if (head.val < x) {
+                    before.next = head;
+                    before = before.next;
+                } else {
+                    after.next = head;
+                    after = after.next;
+                }
+                head = head.next;
+            }
+            after.next = null;
+            before.next = afterHead.next;
+            return beforeHead.next;
+        }
+    }
+}
