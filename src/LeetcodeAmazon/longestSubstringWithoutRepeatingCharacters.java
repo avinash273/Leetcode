0358a459c4445c0650f65c92cc46e35bb5334dcf
avinash273 <avinash.shanker@mavs.uta.edu>
2020-06-30 23:09:24 -0500

    longest Substring Without Repeating Characters

---
+++ src/LeetcodeAmazon/longestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,36 @@
+package LeetcodeAmazon;
+
+import java.util.HashSet;
+
+/**
+ * T: O(n)
+ * S: O(n)
+ */
+public class longestSubstringWithoutRepeatingCharacters {
+    class Solution {
+        public int lengthOfLongestSubstring(String s) {
+            HashSet<Character> set = new HashSet<>();
+            if (s == null || s.length() <= 0)
+                return 0;
+            if (s.length() == 1)
+                return 1;
+            int count = 0;
+            int maxCount = 0;
+            int i = 0;
+
+            while (i < s.length()) {
+                if (!set.contains(s.charAt(i))) {
+                    set.add(s.charAt(i));
+                    count++;
+                } else {
+                    maxCount = Math.max(maxCount, count);
+                    count = 0;
+                    i -= set.size();
+                    set.clear();
+                }
+                i++;
+            }
+            return Math.max(maxCount, count);
+        }
+    }
+}
