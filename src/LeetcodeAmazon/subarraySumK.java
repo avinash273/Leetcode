d602bf74d89cec708d5f298fb33468cf88fdc36e
avinash273 <avinash.shanker@mavs.uta.edu>
2020-07-01 20:47:18 -0500

    subarray sum equals

---
+++ src/LeetcodeAmazon/subarraySumK.java
@@ -0,0 +1,29 @@
+package LeetcodeAmazon;
+
+import java.util.HashMap;
+
+/**
+ * T: O(n)
+ * S: O(n)
+ */
+
+public class subarraySumK {
+    class Solution {
+        public int subarraySum(int[] nums, int k) {
+            int sum = 0;
+            int count = 0;
+
+            HashMap<Integer, Integer> map = new HashMap<>();
+            map.put(0, 1);
+
+            for (int i = 0; i < nums.length; i++) {
+                sum += nums[i];
+
+                if (map.containsKey(sum - k))
+                    count += map.get(sum - k);
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+            return count;
+        }
+    }
+}
