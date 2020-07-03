a6c15939be9c429eca90f3312b2e3a22a394065a
avinash273 <avinash.shanker@mavs.uta.edu>
2020-07-03 18:21:59 -0500

    buddy String

---
+++ src/LeetcodeAmazon/buddyString.java
@@ -0,0 +1,43 @@
+package LeetcodeAmazon;
+
+import java.util.*;
+
+/**
+ * T: O(n)
+ * S: O(n)
+ */
+
+public class buddyString {
+    class Solution {
+        public boolean buddyStrings(String A, String B) {
+            if(A == null || B == null || (A.length() != B.length()))
+                return false;
+            if(A.equals(B)){
+                HashSet<Character> uniqueChars = new HashSet<>();
+                for(char c: A.toCharArray())
+                    uniqueChars.add(c);
+
+                if(uniqueChars.size() < A.length())
+                    return true;
+                else
+                    return false;
+            }
+
+            List<Integer> diff = new ArrayList<>();
+
+            for(int i = 0 ; i < A.length(); i++){
+                if(A.charAt(i) != B.charAt(i)){
+                    diff.add(i);
+                }
+            }
+
+            if(diff.size() == 2
+                    && A.charAt(diff.get(0)) == B.charAt(diff.get(1))
+                    && B.charAt(diff.get(0)) == A.charAt(diff.get(1)))
+                return true;
+            else
+                return false;
+
+        }
+    }
+}
