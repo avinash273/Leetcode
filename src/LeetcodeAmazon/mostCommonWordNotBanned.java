57cca88a65c1a121c2866f7eb8aab62fc3fe8ce4
avinash273 <avinash.shanker@mavs.uta.edu>
2020-07-02 22:00:52 -0500

    most Common Word Not Banned

---
+++ src/LeetcodeAmazon/mostCommonWordNotBanned.java
@@ -0,0 +1,35 @@
+package LeetcodeAmazon;
+
+import java.util.*;
+
+public class mostCommonWordNotBanned {
+    class Solution {
+        public String mostCommonWord(String paragraph, String[] banned) {
+            HashSet<String> banSet = new HashSet<>();
+
+            for (String word : banned)
+                banSet.add(word.toLowerCase());
+
+            HashMap<String, Integer> map = new HashMap<>();
+            String[] goodString = paragraph.toLowerCase().split("\\W+");
+
+            for (String word : goodString) {
+                if (!banSet.contains(word))
+                    map.put(word, map.getOrDefault(word, 0) + 1);
+            }
+
+            int maxCount = 0;
+            String result = "";
+
+            for (String key : map.keySet()) {
+                if (maxCount < map.get(key)) {
+                    maxCount = map.get(key);
+                    result = key;
+                }
+            }
+
+            return result;
+
+        }
+    }
+}
