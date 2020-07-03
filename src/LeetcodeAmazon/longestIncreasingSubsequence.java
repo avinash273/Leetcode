e3079e6d96df5ad44dca0f6c0eca342702fc913c
avinash273 <avinash.shanker@mavs.uta.edu>
2020-07-03 15:12:24 -0500

    longest increasing subsequence, with binary search

--- src/LeetcodeAmazon/longestIncreasingSubsequence.java
+++ src/LeetcodeAmazon/longestIncreasingSubsequence.java
@@ -7,7 +7,27 @@ public class longestIncreasingSubsequence {
     /**
      * Time : O(n log n)
      * Time: O(n)
+     * dynamic prog with binary search
      */
+    class Solution {
+        public int lengthOfLIS(int[] nums) {
+            if(nums == null || nums.length == 0)
+                return 0;
+            int len = 0;
+            int[] dp =  new int[nums.length];
+
+            for(int num : nums){
+                int index = Arrays.binarySearch(dp, 0, len, num);
+                if(index < 0)
+                    index = -(index + 1);
+                dp[index] = num;
+                if(index == len)
+                    len++;
+            }
+            return len;
+        }
+    }
+
 
     /**
      * Time : O(n^2)
