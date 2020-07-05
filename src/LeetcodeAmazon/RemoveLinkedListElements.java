4ec4cf0ebeefb14d945c835dd8d907ad0f5c05f6
avinash273 <avinash.shanker@mavs.uta.edu>
2020-07-05 17:47:48 -0500

    Remove Linked List Elements

---
+++ src/LeetcodeAmazon/RemoveLinkedListElements.java
@@ -0,0 +1,42 @@
+package LeetcodeAmazon;
+
+/**
+ * T: O(n)
+ * S: O(1)
+ */
+public class RemoveLinkedListElements {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    class Solution {
+        public ListNode removeElements(ListNode head, int val) {
+            while (head != null && head.val == val)
+                head = head.next;
+
+            ListNode current = head;
+
+            while (current != null && current.next != null) {
+                if (current.next.val == val)
+                    current.next = current.next.next;
+                else
+                    current = current.next;
+            }
+            return head;
+        }
+    }
+}
