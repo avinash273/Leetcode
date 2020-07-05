a63956c43c7900aea2e55670b0c62ac0a14145e5
avinash273 <avinash.shanker@mavs.uta.edu>
2020-07-05 17:41:45 -0500

    Longest Palindrome

---
+++ src/LeetcodeAmazon/LongestPalindrome.java
@@ -0,0 +1,21 @@
+package LeetcodeAmazon;
+
+public class LongestPalindrome {
+    class Solution {
+        public int longestPalindrome(String s) {
+            int[] charCounts = new int[128];
+            int result = 0;
+
+            for (char c : s.toCharArray())
+                charCounts[c]++;
+            for (int count : charCounts) {
+                result += count / 2 * 2;
+                if (result % 2 == 0 && count % 2 == 1) {
+                    result += 1;
+                }
+            }
+
+            return result;
+        }
+    }
+}
