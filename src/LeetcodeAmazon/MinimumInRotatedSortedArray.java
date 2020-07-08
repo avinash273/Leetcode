db2f1819ac3865c0bbe1307cc74793c766b7a178
avinash273 <avinash.shanker@mavs.uta.edu>
2020-07-07 20:28:03 -0500

    Minimum In Rotated Sorted Array

---
+++ src/LeetcodeAmazon/MinimumInRotatedSortedArray.java
@@ -0,0 +1,30 @@
+package LeetcodeAmazon;
+
+/**
+ * T: O(log n)
+ * S: O(1)
+ */
+public class MinimumInRotatedSortedArray {
+    class Solution {
+        public int findMin(int[] nums) {
+            if (nums.length == 0) return -1;
+            if (nums.length == 1) return nums[0];
+
+            int left = 0;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+
+                if (mid > 0 && nums[mid] < nums[mid - 1]) {
+                    return nums[mid];
+                } else if (nums[left] <= nums[mid] && nums[mid] > nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            return nums[left];
+        }
+    }
+}
