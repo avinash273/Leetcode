090532abd655162a638eb70ca3daf29dccdf3574
avinash273 <avinash.shanker@mavs.uta.edu>
2020-07-25 19:17:13 -0500

    productExceptSelf

--- src/Top75/productExceptSelf.java
+++ src/Top75/productExceptSelf.java
@@ -1,5 +1,9 @@
 package Top75;
 
+/**
+ * T: O(n)
+ * S: O(1)
+ */
 public class productExceptSelf {
     class Solution {
         public int[] productExceptSelf(int[] nums) {
@@ -7,15 +11,15 @@ public class productExceptSelf {
             int[] result = new int[len];
 
             int left = 1;
-            for(int i = 0; i < len; i++){
-                if(i > 0)
+            for (int i = 0; i < len; i++) {
+                if (i > 0)
                     left = left * nums[i - 1];
                 result[i] = left;
             }
 
             int right = 1;
-            for(int i = len - 1; i >= 0; i--){
-                if(i < len - 1)
+            for (int i = len - 1; i >= 0; i--) {
+                if (i < len - 1)
                     right = right * nums[i + 1];
                 result[i] *= right;
             }
