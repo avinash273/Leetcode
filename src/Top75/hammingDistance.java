08f7467f9369d0ca967e3e7b2805d19c2c342b32
avinash273 <avinash.shanker@mavs.uta.edu>
2020-07-27 16:13:09 -0500

    hamming distance

---
+++ src/Top75/hammingDistance.java
@@ -0,0 +1,14 @@
+package Top75;
+
+public class hammingDistance {
+    public class Solution {
+        public int hammingWeight(int n) {
+            int sum = 0;
+            while (n != 0) {
+                sum++;
+                n = n & (n - 1);
+            }
+            return sum;
+        }
+    }
+}
