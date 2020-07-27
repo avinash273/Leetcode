2de2079e812e0d98bc5d911763f3e280764fc0fb
avinash273 <avinash.shanker@mavs.uta.edu>
2020-07-27 15:54:56 -0500

    getSumXOR

---
+++ src/Top75/getSumXOR.java
@@ -0,0 +1,15 @@
+package Top75;
+
+public class getSumXOR {
+    class Solution {
+        public int getSum(int a, int b) {
+            while (b != 0) {
+                int answer = a ^ b;
+                int carry = (a & b) << 1;
+                a = answer;
+                b = carry;
+            }
+            return a;
+        }
+    }
+}
