2d04ebc6f5c04a7f848023f67964086dcc947c1f
avinash273 <avinash.shanker@mavs.uta.edu>
2020-07-27 12:35:07 -0500

    max area

---
+++ src/Top75/maxArea.java
@@ -0,0 +1,18 @@
+package Top75;
+
+public class maxArea {
+    class Solution {
+        public int maxArea(int[] height) {
+            int left = 0;
+            int right = height.length - 1;
+            int maxArea = 0;
+
+            while (left < right) {
+                maxArea = Math.max(maxArea, Math.min(height[left], height[right]) * (right - left));
+                if (height[left] < height[right]) left++;
+                else right--;
+            }
+            return maxArea;
+        }
+    }
+}
