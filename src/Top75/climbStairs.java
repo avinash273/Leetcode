3513e830c12d10b2e28e6f3c282c16d5cfc30f1e
avinash273 <avinash.shanker@mavs.uta.edu>
2020-07-27 22:06:55 -0500

    climbing stairs as factorial

--- src/Top75/climbStairs.java
+++ src/Top75/climbStairs.java
@@ -18,4 +18,20 @@ public class climbStairs {
             return dp[n];
         }
     }
+
+    public class Solution2 {
+        public int climbStairs(int n) {
+            if (n == 1) {
+                return 1;
+            }
+            int first = 1;
+            int second = 2;
+            for (int i = 3; i <= n; i++) {
+                int third = first + second;
+                first = second;
+                second = third;
+            }
+            return second;
+        }
+    }
 }
