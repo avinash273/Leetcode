6319d89b43ca6352bce4eca4fdfa8ffbc41052c9
avinash273 <avinash.shanker@mavs.uta.edu>
2020-07-28 21:00:26 -0500

    coinChange

--- src/Top75/coinChange.java
+++ src/Top75/coinChange.java
@@ -6,6 +6,10 @@ import java.util.Arrays;
  * T: O(m * n)
  * S: O(n)
  */
+
+/**
+ * updated one after trying sort
+ */
 public class coinChange {
     class Solution {
         public int coinChange(int[] coins, int amount) {
@@ -15,10 +19,31 @@ public class coinChange {
             //bottom up processing
             dp[0] = 0;
 
-            for(int i = 0; i <= amount; i++){
-                for(int j = 0; j < coins.length; j++){
-                    if(coins[j] <= i){
+            for (int i = 0; i <= amount; i++) {
+                for (int j = 0; j < coins.length; j++) {
+                    if (coins[j] <= i) {
                         dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
+                    }
+                }
+            }
+            return dp[amount] > amount ? -1 : dp[amount];
+        }
+    }
+
+    class Solution2 {
+        public int coinChange(int[] coins, int amount) {
+            int[] dp = new int[amount + 1];
+            Arrays.fill(dp, amount + 1);
+            Arrays.sort(coins);
+            //bottom up processing
+            dp[0] = 0;
+
+            for (int i = 0; i <= amount; i++) {
+                for (int j = 0; j < coins.length; j++) {
+                    if (coins[j] <= i) {
+                        dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
+                    } else {
+                        break;
                     }
                 }
             }
