59eb141b82d7c9b1fd973f22b28a0dc4f28a774e
avinash273 <avinash.shanker@mavs.uta.edu>
2020-07-29 23:49:27 -0500

    word break

---
+++ src/Top75/wordBreak.java
@@ -0,0 +1,24 @@
+package Top75;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class wordBreak {
+    class Solution {
+        public boolean wordBreak(String s, List<String> wordDict) {
+            Set<String> wordDictSet = new HashSet(wordDict);
+            boolean[] dp = new boolean[s.length() + 1];
+            dp[0] = true;
+
+            for (int i = 1; i <= s.length(); i++) {
+                for (int j = 0; j < i; j++) {
+                    if (dp[j] == true && wordDictSet.contains(s.substring(j, i))) {
+                        dp[i] = true;
+                    }
+                }
+            }
+            return dp[s.length()];
+        }
+    }
+}
