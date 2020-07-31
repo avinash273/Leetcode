62970fbf8d62d728e9d2cee020570c2005df516e
avinash273 <avinash.shanker@mavs.uta.edu>
2020-07-31 16:22:13 -0500

    valid graph

--- src/Top75/validGraph.java
+++ src/Top75/validGraph.java
@@ -3,6 +3,43 @@ package Top75;
 import java.util.*;
 
 public class validGraph {
+
+    class Solution {
+        public boolean validTree(int n, int[][] edges) {
+            if (edges.length != n - 1) return false;
+
+            //Make adjacency list
+            List<List<Integer>> adjacencyList = new ArrayList<>();
+            for (int i = 0; i < n; i++)
+                adjacencyList.add(new ArrayList<>());
+
+            for (int[] edge : edges) {
+                adjacencyList.get(edge[0]).add(edge[1]);
+                adjacencyList.get(edge[1]).add(edge[0]);
+            }
+
+            System.out.println(adjacencyList);
+
+            Stack<Integer> stack = new Stack<>();
+            Set<Integer> seen = new HashSet<>();
+            stack.push(0);
+            seen.add(0);
+
+            while (!stack.isEmpty()) {
+                int node = stack.pop();
+                // System.out.println(node);
+                for (int neighbour : adjacencyList.get(node)) {
+                    // System.out.println(neighbour +" "+adjacencyList.get(node));
+                    if (seen.contains(neighbour)) continue;
+                    seen.add(neighbour);
+                    stack.push(neighbour);
+                }
+            }
+
+            return seen.size() == n;
+        }
+    }
+
     class Solution1 {
         public boolean validTree(int n, int[][] edges) {
 
