4b1096da4cc9ef5dd21cc37706c7b37f6ca354a2
avinash273 <avinash.shanker@mavs.uta.edu>
2020-08-01 01:09:32 -0500

    minMeetingRooms

---
+++ src/Top75/minMeetingRooms.java
@@ -0,0 +1,32 @@
+package Top75;
+
+public class minMeetingRooms {
+    class Solution {
+        public int minMeetingRooms(int[][] intervals) {
+
+            int n = intervals.length;
+            int[] start = new int[n];
+            int[] end = new int[n];
+
+            for(int i = 0; i < n; i++){
+                start[i] = intervals[i][0];
+                end[i] = intervals[i][1];
+            }
+
+            Arrays.sort(start);
+            Arrays.sort(end);
+
+            int startInd = 0, endInd = 0, rooms = 0;
+
+            while(startInd < n){
+
+                if(start[startInd] < end[endInd])
+                    rooms++;
+                else
+                    endInd++;
+                startInd++;
+            }
+            return rooms;
+        }
+    }
+}
