90a62cf56babae9c1d60fbcadd1df954ea000490
avinash273 <avinash.shanker@mavs.uta.edu>
2020-08-01 00:54:09 -0500

    meeting rooms

---
+++ src/Top75/meetingRooms.java
@@ -0,0 +1,20 @@
+package Top75;
+
+import java.util.Arrays;
+
+public class meetingRooms {
+    class Solution {
+        public boolean canAttendMeetings(int[][] intervals) {
+            if (intervals == null) return false;
+
+            Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+            for (int i = 0; i < intervals.length - 1; i++) {
+                if (intervals[i][1] > intervals[i + 1][0])
+                    return false;
+            }
+            return true;
+        }
+    }
+
+}
