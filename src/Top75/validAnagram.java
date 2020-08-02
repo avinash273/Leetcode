760895a209ac09ba3a4fb906aad4d26578e60260
avinash273 <avinash.shanker@mavs.uta.edu>
2020-08-02 13:20:39 -0500

    validAnagram

---
+++ src/Top75/validAnagram.java
@@ -0,0 +1,22 @@
+package Top75;
+
+public class validAnagram {
+    class Solution {
+        public boolean isAnagram(String s, String t) {
+            if (s.length() != t.length()) return false;
+
+            int[] charMap = new int[26];
+
+            for (int i = 0; i < s.length(); i++) {
+                charMap[s.charAt(i) - 'a']++;
+                charMap[t.charAt(i) - 'a']--;
+            }
+
+            for (int k : charMap) {
+                if (k != 0) return false;
+            }
+            return true;
+        }
+    }
+
+}
