39f8fa0cdd791e9dea200121c87fd6df40222253
avinash273 <avinash.shanker@mavs.uta.edu>
2020-08-14 17:07:35 -0500

    error handling

---
+++ src/ErrorHandling/ErrorHandling.java
@@ -0,0 +1,21 @@
+package ErrorHandling;
+
+public class ErrorHandling {
+    public static void main(String[] args) {
+
+        String s = "";
+        try {
+            var sub = s.substring(1);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            System.out.println("Null pointer: " + e.getMessage());
+        } catch (StringIndexOutOfBoundsException e) {
+            e.printStackTrace();
+            System.out.println("Out of bounds: " + e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("I don't know what happened: " + e.getMessage());
+        }
+        System.out.println("I'm not dead yet!");
+    }
+}
