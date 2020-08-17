8b1b43a771a99de0dd0d87c2d41fbc017e606b20
avinash273 <avinash.shanker@mavs.uta.edu>
2020-08-16 19:39:05 -0500

    customer class, iterable class usage

---
+++ src/IterableClassUsage/Customer.java
@@ -0,0 +1,18 @@
+package IterableClassUsage;
+
+public class Customer {
+    private String name;
+
+    public Customer(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String toString(){
+        return name;
+    }
+
+}
---
+++ src/IterableClassUsage/CustomerRecords.java
@@ -0,0 +1,4 @@
+package IterableClassUsage;
+
+public class CustomerRecords {
+}
