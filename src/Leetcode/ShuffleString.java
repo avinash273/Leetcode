29a3ba8c5aed11e66461d0f460ee71d0cd9d1897
avinash273 <avinash.shanker@mavs.uta.edu>
2020-08-18 02:06:54 -0500

    shuffle string

---
+++ src/Leetcode/ShuffleString.java
@@ -0,0 +1,13 @@
+package Leetcode;
+
+public class ShuffleString {
+    class Solution {
+        public String restoreString(String s, int[] indices) {
+            char[] result = new char[s.length()];
+            for (int i = 0; i < s.length(); i++) {
+                result[indices[i]] = s.charAt(i);
+            }
+            return new String(result);
+        }
+    }
+}
