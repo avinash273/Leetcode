d6ce50fa04803ffd0972088398a69ea48d12af12
avinash273 <avinash.shanker@mavs.uta.edu>
2020-08-18 01:03:12 -0500

    toLowerCase

---
+++ src/Leetcode/ToLowerCase.java
@@ -0,0 +1,15 @@
+package Leetcode;
+
+public class ToLowerCase {
+    class Solution {
+        public String toLowerCase(String str) {
+            //return str.toLowerCase();
+            char[] a = str.toCharArray();
+            for (int i = 0; i < a.length; i++) {
+                if (a[i] >= 'A' && a[i] <= 'Z')
+                    a[i] = (char) (a[i] - 'A' + 'a');
+            }
+            return new String(a);
+        }
+    }
+}
