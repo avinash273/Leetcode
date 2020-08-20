fe1d4618455decef6628d7615fb65e71796535ee
avinash273 <avinash.shanker@mavs.uta.edu>
2020-08-20 15:16:46 -0500

    ValidParentheses20

---
+++ src/PracticeYoutuber/Stack/ValidParentheses20.java
@@ -0,0 +1,21 @@
+package PracticeYoutuber.Stack;
+
+import java.util.Stack;
+
+public class ValidParentheses20 {
+    //https://leetcode.com/problems/valid-parentheses/discuss/9178/Short-java-solution
+    class Solution {
+        public boolean isValid(String s) {
+            Stack<Character> stack = new Stack<>();
+            for (char bracket : s.toCharArray()) {
+                if (bracket == '{') stack.push('}');
+                else if (bracket == '[') stack.push(']');
+                else if (bracket == '(') stack.push(')');
+                else if (stack.isEmpty() || stack.pop() != bracket) return false;
+            }
+            return stack.isEmpty();
+        }
+    }
+
+
+}
