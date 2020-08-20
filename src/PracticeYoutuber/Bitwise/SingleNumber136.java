7e44edd93e6048adaba75140f97ceac32d239b20
avinash273 <avinash.shanker@mavs.uta.edu>
2020-08-20 16:08:17 -0500

    SingleNumber136

---
+++ src/PracticeYoutuber/Bitwise/SingleNumber136.java
@@ -0,0 +1,12 @@
+package PracticeYoutuber.Bitwise;
+
+public class SingleNumber136 {
+    class Solution {
+        public int singleNumber(int[] nums) {
+            int a = 0;
+            for (int num : nums)
+                a = a ^ num;
+            return a;
+        }
+    }
+}
