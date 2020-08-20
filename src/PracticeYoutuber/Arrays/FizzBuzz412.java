5669a2d747424068d9e292f07d695b06fb90aa39
avinash273 <avinash.shanker@mavs.uta.edu>
2020-08-20 17:48:22 -0500

    FizzBuzz412 faster solution

--- src/PracticeYoutuber/Arrays/FizzBuzz412.java
+++ src/PracticeYoutuber/Arrays/FizzBuzz412.java
@@ -21,4 +21,40 @@ public class FizzBuzz412 {
             return result;
         }
     }
+
+    /**
+     * T: O(n)
+     * S: O(1)
+     * faster solution
+     */
+    class Solution2 {
+        public List<String> fizzBuzz(int n) {
+            List<String> result = new ArrayList<>();
+
+            for(int i = 1, fizz = 0, buzz = 0; i <= n; i++){
+                fizz++;
+                buzz++;
+
+                if(fizz == 3 && buzz == 5){
+                    result.add("FizzBuzz");
+                    fizz = 0;
+                    buzz = 0;
+                }
+                else if(fizz == 3){
+                    result.add("Fizz");
+                    fizz = 0;
+                }
+                else if(buzz == 5){
+                    result.add("Buzz");
+                    buzz = 0;
+                }
+                else{
+                    result.add(String.valueOf(i));
+                }
+            }
+            return result;
+        }
+    }
+
+
 }
