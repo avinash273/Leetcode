6c4de2d073e65206c707455f8305ebf3651418f0
avinash273 <avinash.shanker@mavs.uta.edu>
2020-08-21 13:55:29 -0500

    Reverse Integer

---
+++ src/PracticeYoutuber/Arrays/ReverseInteger.java
@@ -0,0 +1,28 @@
+package PracticeYoutuber.Arrays;
+
+/**
+ * T: O(log10 n)
+ * S: (1)
+ */
+public class ReverseInteger {
+    class Solution {
+        public int reverse(int num) {
+            boolean negative = false;
+            if (num < 0) {
+                negative = true;
+                num = num * -1;
+            }
+
+            long reversed = 0;
+
+            while (num > 0) {
+                reversed = (reversed * 10) + (num % 10);
+                num = num / 10;
+            }
+
+            if (reversed > Integer.MAX_VALUE) return 0;
+
+            return negative ? (int) (-1 * reversed) : (int) reversed;
+        }
+    }
+}
