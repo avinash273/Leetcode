3904ef9769662e10db0cf16b64e27bbb1f7882d3
avinash273 <avinash.shanker@mavs.uta.edu>
2020-08-20 23:18:11 -0500

    BuySellStock121

---
+++ src/PracticeYoutuber/Arrays/BuySellStock121.java
@@ -0,0 +1,16 @@
+package PracticeYoutuber.Arrays;
+
+public class BuySellStock121 {
+    class Solution {
+        public int maxProfit(int[] prices) {
+            int min = Integer.MAX_VALUE;
+            int profit = 0;
+
+            for (int price : prices) {
+                if (price < min) min = price;
+                else if (price - min > profit) profit = price - min;
+            }
+            return profit;
+        }
+    }
+}
