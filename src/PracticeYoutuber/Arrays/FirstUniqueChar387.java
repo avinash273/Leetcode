d20673ea4e76470da861e5148abdc0799f624b23
avinash273 <avinash.shanker@mavs.uta.edu>
2020-08-20 23:29:49 -0500

    FirstUniqueChar387

---
+++ src/PracticeYoutuber/Arrays/FirstUniqueChar387.java
@@ -0,0 +1,19 @@
+package PracticeYoutuber.Arrays;
+
+public class FirstUniqueChar387 {
+    class Solution {
+        public int firstUniqChar(String s) {
+            int[] alphabet = new int[26];
+
+            for (int i = 0; i < s.length(); i++) {
+                alphabet[s.charAt(i) - 'a']++;
+            }
+
+            for (int i = 0; i < s.length(); i++) {
+                if (alphabet[s.charAt(i) - 'a'] == 1)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
