16703f9d3980f94822ec3fb23bb43a18777693a2
avinash273 <avinash.shanker@mavs.uta.edu>
2020-08-22 19:05:16 -0500

    ContainsDuplicateII_219

---
+++ src/PracticeYoutuber/Arrays/ContainsDuplicateII_219.java
@@ -0,0 +1,24 @@
+package PracticeYoutuber.Arrays;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContainsDuplicateII_219 {
+    class Solution {
+        public boolean containsNearbyDuplicate(int[] nums, int k) {
+            Map<Integer, Integer> map = new HashMap<>();
+
+            int index = 0;
+            for (int each : nums) {
+                if (map.containsKey(each)) {
+                    if ((index - map.get(each)) <= k) return true;
+                }
+                map.put(each, index);
+                index++;
+            }
+            return false;
+        }
+    }
+
+
+}
