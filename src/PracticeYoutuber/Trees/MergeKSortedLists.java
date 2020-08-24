bfc427fc9666454947feacb47ed91889889bb10e
avinash273 <avinash.shanker@mavs.uta.edu>
2020-08-24 15:51:24 -0500

    merge k sorted lists

---
+++ src/PracticeYoutuber/Trees/MergeKSortedLists.java
@@ -0,0 +1,45 @@
+package PracticeYoutuber.Trees;
+
+import java.util.PriorityQueue;
+
+public class MergeKSortedLists {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    class Solution {
+        public ListNode mergeKLists(ListNode[] lists) {
+            PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+            for (ListNode list : lists) {
+                while (list != null) {
+                    minHeap.add(list.val);
+                    list = list.next;
+                }
+            }
+
+            ListNode head = new ListNode(-1);
+            ListNode returnHead = head;
+
+            while (!minHeap.isEmpty()) {
+                head.next = new ListNode(minHeap.remove());
+                head = head.next;
+            }
+            return returnHead.next;
+        }
+    }
+}
