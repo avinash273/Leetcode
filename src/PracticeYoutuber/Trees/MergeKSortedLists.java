1c08026fe8f1008f046770decdcab9e549426769
avinash273 <avinash.shanker@mavs.uta.edu>
2020-08-30 17:54:44 -0500

    Merge K Sorted Lists retry

--- src/PracticeYoutuber/Trees/MergeKSortedLists.java
+++ src/PracticeYoutuber/Trees/MergeKSortedLists.java
@@ -47,4 +47,36 @@ public class MergeKSortedLists {
             return returnHead.next;
         }
     }
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution2 {
+        public ListNode mergeKLists(ListNode[] lists) {
+            PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+            for(ListNode node : lists){
+                while(node != null){
+                    minHeap.add(node.val);
+                    node = node.next;
+                }
+            }
+
+            ListNode head = new ListNode(-1);
+            ListNode dummy = head;
+
+            while(!minHeap.isEmpty()){
+                head.next = new ListNode(minHeap.remove());
+                head = head.next;
+            }
+            return dummy.next;
+        }
+    }
 }
