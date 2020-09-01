d8ae660cbb2f0f830a894cd536ddc0d10aceea80
avinash273 <avinash.shanker@mavs.uta.edu>
2020-08-31 23:32:28 -0500

    LongestSubstringWithoutRepeatingCharacters solution 2

--- src/PracticeYoutuber/Arrays/LongestSubstringWithoutRepeatingCharacters.java
+++ src/PracticeYoutuber/Arrays/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,5 +1,6 @@
 package PracticeYoutuber.Arrays;
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 /**
@@ -29,6 +30,23 @@ public class LongestSubstringWithoutRepeatingCharacters {
                 maxCount = Math.max(maxCount, count);
             }
             return maxCount;
+        }
+    }
+
+    class Solution2 {
+        public int lengthOfLongestSubstring(String s) {
+            HashMap<Character, Integer> map = new HashMap<>();
+
+            int max = 0;
+
+            for (int i = 0, j = 0; i < s.length(); i++) {
+                if (map.containsKey(s.charAt(i)))
+                    j = Math.max(j, map.get(s.charAt(i)) + 1);
+
+                map.put(s.charAt(i), i);
+                max = Math.max(max, i - j + 1);
+            }
+            return max;
         }
     }
 }
