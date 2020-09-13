4937086cab3dd6e31881a55ecb4433a260fced62
avinash273 <avinash.shanker@mavs.uta.edu>
2020-09-13 01:29:29 -0500

    numIslands

--- src/PracticeYoutuber/DFS/NumIslands200.java
+++ src/PracticeYoutuber/DFS/NumIslands200.java
@@ -36,4 +36,36 @@ public class NumIslands200 {
             return 1;
         }
     }
+
+    class Solution2 {
+        public int numIslands(char[][] grid) {
+            int numIslands = 0;
+
+            for(int i = 0; i < grid.length; i++){
+                for(int j = 0; j < grid[i].length; j++){
+                    if(grid[i][j] == '1')
+                        numIslands += DFS(grid, i, j);
+                }
+            }
+            return numIslands;
+        }
+
+        public int DFS(char[][] grid, int i, int j){
+            if(i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] == '0') return 0;
+
+            grid[i][j] = '0';
+            //down
+            DFS(grid, i + 1, j);
+            //up
+            DFS(grid, i - 1, j);
+            //left
+            DFS(grid, i, j - 1);
+            //right
+            DFS(grid, i, j + 1);
+            return 1;
+        }
+    }
+
+
+
 }
