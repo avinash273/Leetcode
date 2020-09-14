f4ea3a50e6ab217c755a9baba77703f4b093830f
avinash273 <avinash.shanker@mavs.uta.edu>
2020-09-13 20:42:10 -0500

    power of two using bitwise

--- src/PracticeYoutuber/DFS/NumIslands200.java
+++ src/PracticeYoutuber/DFS/NumIslands200.java
@@ -3,7 +3,9 @@ package PracticeYoutuber.DFS;
 /**
  * T: O(n)
  * S: O(n)
+ * done it three times
  */
+
 public class NumIslands200 {
     class Solution {
         public int numIslands(char[][] grid) {
@@ -62,6 +64,36 @@ public class NumIslands200 {
             DFS(grid, i, j - 1);
             //right
             DFS(grid, i, j + 1);
+            return 1;
+        }
+    }
+
+    class Solution3 {
+        public int numIslands(char[][] grid) {
+            if(grid == null || grid.length == 0) return 0;
+
+            int numIslands = 0;
+            for(int i = 0; i < grid.length; i++){
+                for(int j = 0; j < grid[i].length; j++){
+                    if(grid[i][j] == '1')
+                        numIslands += DFS(grid, i, j);
+                }
+            }
+            return numIslands;
+        }
+
+        public int DFS(char[][] grid, int i, int j){
+            if(i < 0 || j < 0 || i >= grid.length || j >= grid[i].length || grid[i][j] == '0') return 0;
+
+            grid[i][j] = '0';
+            //down
+            DFS(grid, i + 1, j);
+            //up
+            DFS(grid, i - 1, j);
+            //right
+            DFS(grid, i, j + 1);
+            //left
+            DFS(grid, i, j - 1);
             return 1;
         }
     }
