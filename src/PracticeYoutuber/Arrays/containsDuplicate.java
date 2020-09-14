47b8bf7b91408cfbb542187cd74a0f1dab10253f
avinash273 <avinash.shanker@mavs.uta.edu>
2020-09-13 21:02:58 -0500

    containsDuplicate

---
+++ src/PracticeYoutuber/Arrays/containsDuplicate.java
@@ -0,0 +1,19 @@
+package PracticeYoutuber.Arrays;
+
+import java.util.HashSet;
+
+public class containsDuplicate {
+    class Solution {
+        public boolean containsDuplicate(int[] nums) {
+            HashSet<Integer> set = new HashSet<>();
+
+            for (int num : nums) {
+                if (set.contains(num)) return true;
+                else
+                    set.add(num);
+            }
+            return false;
+
+        }
+    }
+}
