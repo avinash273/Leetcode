de3c04778f9a0e4520f51362794e0313b38bacfc
avinash273 <avinash.shanker@mavs.uta.edu>
2020-09-13 21:56:20 -0500

    isAnagram

---
+++ src/PracticeYoutuber/Arrays/isAnagram.java
@@ -0,0 +1,24 @@
+package PracticeYoutuber.Arrays;
+
+public class isAnagram {
+    class Solution {
+        public boolean isAnagram(String s, String t) {
+            if (s.length() != t.length()) return false;
+
+            int[] charMap = new int[26];
+
+            for (int i = 0; i < s.length(); i++) {
+                charMap[s.charAt(i) - 'a']++;
+                charMap[t.charAt(i) - 'a']--;
+            }
+
+            for (int map : charMap)
+                if (map != 0) return false;
+
+            return true;
+
+        }
+    }
+
+
+}
