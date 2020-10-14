06daacdf5f65ce76f37789d707f29cec9e7aa742
avinash273 <avinash.shanker@mavs.uta.edu>
2020-10-14 18:46:37 -0500

    ValidParantheses

---
+++ src/YoutuberKevin/ValidParantheses.java
@@ -0,0 +1,25 @@
+package YoutuberKevin;
+
+import java.util.Stack;
+
+public class ValidParantheses {
+    class Solution {
+        public boolean isValid(String s) {
+            if (s == null || s.length() == 0) return false;
+
+            Stack<Character> stack = new Stack<>();
+
+            for (char brace : s.toCharArray()) {
+                if (brace == '{')
+                    stack.push('}');
+                else if (brace == '(')
+                    stack.push(')');
+                else if (brace == '[')
+                    stack.push(']');
+                else if (stack.isEmpty() || stack.pop() != brace)
+                    return false;
+            }
+            return stack.isEmpty();
+        }
+    }
+}
