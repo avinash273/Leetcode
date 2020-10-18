98c29ef3d97ad40f2873aab02405f7bc27ca1adb
avinash273 <avinash.shanker@mavs.uta.edu>
2020-10-18 08:25:57 -0500

    Single XOR rep

---
+++ src/YoutuberKevin/SingleNumberXOR.java
@@ -0,0 +1,17 @@
+package YoutuberKevin;
+
+/**
+ * T: O(n)
+ * S: O(1)
+ */
+public class SingleNumberXOR {
+    class Solution {
+        public int singleNumber(int[] nums) {
+            int a = 0;
+            for (int num : nums) {
+                a ^= num;
+            }
+            return a;
+        }
+    }
+}
