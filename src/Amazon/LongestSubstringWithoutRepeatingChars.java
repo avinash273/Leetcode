a4b7ca9c0d5273ddb42fda07476a9bc253964417
avinash273 <avinash.shanker@mavs.uta.edu>
2020-10-23 15:18:33 -0500

    LongestSubstringWithoutRepeatingChars

--- src/Amazon/LongestSubstringWithoutRepeatingChars.java
+++ src/Amazon/LongestSubstringWithoutRepeatingChars.java
@@ -1,5 +1,6 @@
 package Amazon;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -31,6 +32,26 @@ public class LongestSubstringWithoutRepeatingChars {
                 }
             }
             return maxValue;
+        }
+    }
+
+    /**
+     * T: O(n)
+     * S: O(n)
+     */
+    class Solution2 {
+        public int lengthOfLongestSubstring(String s) {
+            HashMap<Character, Integer> map = new HashMap<>();
+            int max = 0;
+
+            for(int i = 0, j = 0; i < s.length(); i++){
+                if(map.containsKey(s.charAt(i))){
+                    j = Math.max(j, map.get(s.charAt(i)) + 1);
+                }
+                map.put(s.charAt(i), i);
+                max = Math.max(max, i - j + 1);
+            }
+            return max;
         }
     }
 }
