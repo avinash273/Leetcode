52efe8ba796333d3e38dfe3b6831f7792ea4f6ad
avinash273 <avinash.shanker@mavs.uta.edu>
2020-10-25 16:17:19 -0500

    MedianTwoSortedArrays binary search

--- src/Amazon/MedianTwoSortedArrays.java
+++ src/Amazon/MedianTwoSortedArrays.java
@@ -3,6 +3,47 @@ package Amazon;
 import java.util.Arrays;
 
 public class MedianTwoSortedArrays {
+
+    /**
+     * T: O(log(min(m,n)))
+     * S: O(1)
+     */
+    class Solution {
+        public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+            if (nums1.length > nums2.length)
+                return findMedianSortedArrays(nums2, nums1);
+
+            int x = nums1.length;
+            int y = nums2.length;
+
+            int left = 0;
+            int right = x;
+
+            while (left <= right) {
+                int partitionX = (left + right) / 2;
+                int partitionY = (x + y + 1) / 2 - partitionX;
+
+                int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : nums1[partitionX - 1];
+                int minRightX = (partitionX == x) ? Integer.MAX_VALUE : nums1[partitionX];
+
+                int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : nums2[partitionY - 1];
+                int minRightY = (partitionY == y) ? Integer.MAX_VALUE : nums2[partitionY];
+
+                if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                    if ((x + y) % 2 == 0)
+                        return (Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY)) / 2.0;
+                    else
+                        return Math.max(maxLeftX, maxLeftY);
+                } else if (maxLeftX > minRightY)
+                    right = partitionX - 1;
+                else
+                    left = partitionX + 1;
+
+            }
+            return 0.0;
+        }
+    }
+
     /**
      * T: O(m+n)
      * S: O(m+n)
