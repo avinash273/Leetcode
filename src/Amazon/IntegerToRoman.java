f5e4b4544be0a4b0040bb24d17e7b499c68f0558
avinash273 <avinash.shanker@mavs.uta.edu>
2020-10-28 13:35:00 -0500

    public class IntegerToRoman {

---
+++ src/Amazon/IntegerToRoman.java
@@ -0,0 +1,23 @@
+package Amazon;
+
+/**
+ * T: O(1)
+ * S: O(1)
+ */
+public class IntegerToRoman {
+    class Solution {
+        public String intToRoman(int num) {
+            StringBuilder sb = new StringBuilder();
+            int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+            String[] roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+            for (int i = 0; i < values.length; i++) {
+                while (num >= values[i]) {
+                    num -= values[i];
+                    sb.append(roman[i]);
+                }
+            }
+            return sb.toString();
+        }
+    }
+}
