cc2e9df17dd7dd1136bc96183871867b7e549b9f
avinash273 <avinash.shanker@mavs.uta.edu>
2020-11-01 22:04:05 -0600

    BSTLevelOrderTraversal

--- src/Amazon/BSTLevelOrderTraversal.java
+++ src/Amazon/BSTLevelOrderTraversal.java
@@ -17,9 +17,6 @@ public class BSTLevelOrderTraversal {
         TreeNode left;
         TreeNode right;
 
-        TreeNode() {
-        }
-
         TreeNode(int val) {
             this.val = val;
         }
