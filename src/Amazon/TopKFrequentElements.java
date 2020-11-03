8e5e7018b934af8d00584389e2005fc6f50c93d4
avinash273 <avinash.shanker@mavs.uta.edu>
2020-11-02 19:17:48 -0600

    TopKFrequentElements

--- src/Amazon/TopKFrequentElements.java
+++ src/Amazon/TopKFrequentElements.java
@@ -23,6 +23,7 @@ public class TopKFrequentElements {
                 if (minHeap.size() > k)
                     minHeap.poll();
             }
+
             int[] result = new int[k];
             for (int i = k - 1; i >= 0; i--)
                 result[i] = minHeap.poll();
