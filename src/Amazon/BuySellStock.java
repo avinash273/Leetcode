32278e604848a287ad2240420bdd41a7de2aeef1
avinash273 <avinash.shanker@mavs.uta.edu>
2020-11-02 20:08:33 -0600

    BuySellStock

--- src/Amazon/BuySellStock.java
+++ src/Amazon/BuySellStock.java
@@ -18,3 +18,10 @@ public class BuySellStock {
         }
     }
 }
+
+/**
+ minPrice is the minimum price from day 0 to day i. And maxPro is the maximum profit we can get from day 0 to day i.
+
+ How to get maxPro?
+ Just get the larger one between current maxPro and prices[i] - minPrice.
+ **/
