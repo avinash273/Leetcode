c59707ed21d7882605c3fcd709a8646621de86e9
avinash273 <avinash.shanker@mavs.uta.edu>
2020-11-03 21:39:25 -0600

    FindKthLargest

---
+++ src/Amazon/FindKthLargest.java
@@ -0,0 +1,20 @@
+package Amazon;
+
+import java.util.PriorityQueue;
+
+/**
+ * T: O(n log k)
+ * S: O(k)
+ */
+class FindKthLargest {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        
+        for(int num : nums){
+            minHeap.add(num);
+            if(minHeap.size() > k)
+                minHeap.poll();
+        }
+        return minHeap.peek();
+    }
+}
