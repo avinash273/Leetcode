9c9546cc37675eda6fb17ea35d879b5da8923328
avinash273 <avinash.shanker@mavs.uta.edu>
2020-11-03 21:47:33 -0600

    KClosestPointsToOrigin

---
+++ src/Amazon/KClosestPointsToOrigin.java
@@ -0,0 +1,28 @@
+package Amazon;
+
+import java.util.PriorityQueue;
+
+/**
+ * T: O(n log k)
+ * S: O(k)
+ */
+public class KClosestPointsToOrigin {
+    class Solution {
+        public int[][] kClosest(int[][] points, int K) {
+            PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> (b[0] * b[0] + b[1] * b[1]) - (a[0] * a[0] + a[1] * a[1]));
+
+            for (int[] point : points) {
+                maxHeap.add(point);
+                if (maxHeap.size() > K)
+                    maxHeap.poll();
+            }
+
+            int result[][] = new int[K][2];
+            while (K > 0)
+                result[--K] = maxHeap.poll();
+            return result;
+        }
+    }
+
+
+}
