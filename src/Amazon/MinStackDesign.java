aa4604b9adc242dca47eb03eeedc20fdb05c26e0
avinash273 <avinash.shanker@mavs.uta.edu>
2020-11-04 21:52:57 -0600

    MinStackDesign

---
+++ src/Amazon/MinStackDesign.java
@@ -0,0 +1,35 @@
+package Amazon;
+
+import java.util.Stack;
+
+public class MinStackDesign {
+    class MinStack {
+        private Stack<Integer> stack = new Stack<>();
+        private Stack<Integer> minStack = new Stack<>();
+
+        public MinStack() {
+        }
+
+        public void push(int x) {
+            stack.push(x);
+            if (minStack.isEmpty() || x <= minStack.peek())
+                minStack.push(x);
+        }
+
+        public void pop() {
+            if (stack.peek().equals(minStack.peek()))
+                minStack.pop();
+            stack.pop();
+        }
+
+        public int top() {
+            return stack.peek();
+        }
+
+        public int getMin() {
+            return minStack.peek();
+        }
+    }
+
+
+}
