816b424cce55593df9cb2a235e4222f6d02db015
avinash273 <avinash.shanker@mavs.uta.edu>
2020-11-04 21:57:49 -0600

    BinaryTreeZigZagLevelOrderBFS

--- src/Amazon/BinaryTreeZigZagLevelOrderBFS.java
+++ src/Amazon/BinaryTreeZigZagLevelOrderBFS.java
@@ -1,24 +1,34 @@
 package Amazon;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /**
- *
+ * T: O(n)
  */
 public class BinaryTreeZigZagLevelOrderBFS {
-    /**
-     * Definition for a binary tree node.
-     * public class TreeNode {
-     * int val;
-     * TreeNode left;
-     * TreeNode right;
-     * TreeNode() {}
-     * TreeNode(int val) { this.val = val; }
-     * TreeNode(int val, TreeNode left, TreeNode right) {
-     * this.val = val;
-     * this.left = left;
-     * this.right = right;
-     * }
-     * }
-     */
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
     class Solution {
         public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
             List<List<Integer>> result = new ArrayList<>();
