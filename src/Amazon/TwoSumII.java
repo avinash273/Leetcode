8c421ed6901824c1ca6cfebe9d333256076d7e0d
avinash273 <avinash.shanker@mavs.uta.edu>
2020-11-08 23:19:35 -0600

    TwoSumII

---
+++ src/Amazon/TwoSumII.java
@@ -0,0 +1,25 @@
+package Amazon;
+
+/**
+ * T: O(n)
+ * S: O(1)
+ */
+public class TwoSumII {
+    class Solution {
+        public int[] twoSum(int[] numbers, int target) {
+            int left = 0;
+            int right = numbers.length - 1;
+
+            while (left <= right) {
+                int currentSum = numbers[left] + numbers[right];
+                if (currentSum == target)
+                    return new int[]{left + 1, right + 1};
+                else if (currentSum < target)
+                    left++;
+                else
+                    right--;
+            }
+            return new int[]{0, 0};
+        }
+    }
+}
