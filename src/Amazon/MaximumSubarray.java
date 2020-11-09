06359193e3222e8029f4410799a1bcc41eb5c8df
avinash273 <avinash.shanker@mavs.uta.edu>
2020-11-08 23:31:24 -0600

    MaximumSubarray

---
+++ src/Amazon/MaximumSubarray.java
@@ -0,0 +1,25 @@
+package Amazon;
+
+/**
+ * T: O(n)
+ * S: O(1)
+ */
+public class MaximumSubarray {
+    class Solution {
+        public int maxSubArray(int[] nums) {
+            int n = nums.length;
+            if (nums == null || n == 0) return 0;
+
+            int currSum = nums[0];
+            int maxSum = nums[0];
+
+            for (int i = 1; i < n; i++) {
+                currSum = Math.max(nums[i], nums[i] + currSum);
+                maxSum = Math.max(maxSum, currSum);
+            }
+            return maxSum;
+        }
+    }
+
+
+}
