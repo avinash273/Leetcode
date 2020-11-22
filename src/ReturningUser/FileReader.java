dff63c8175c118f7a94f6bcc81964c46a098a2eb
avinash273 <avinash.shanker@mavs.uta.edu>
2020-11-21 22:03:31 -0600

    file reader

--- src/ReturningUser/FileReader.java
+++ src/ReturningUser/FileReader.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
 import java.util.Set;
 
