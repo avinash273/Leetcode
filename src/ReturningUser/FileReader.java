cb33562ff07c63a8d0f08a8f823e5bec2ebc75bc
avinash273 <avinash.shanker@mavs.uta.edu>
2020-11-26 18:27:29 -0600

    file reader, efficient

--- src/ReturningUser/FileReader.java
+++ src/ReturningUser/FileReader.java
@@ -1,9 +1,11 @@
 package ReturningUser;
 
 import java.io.BufferedReader;
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
 
 //what are states in java
 public class FileReader {
