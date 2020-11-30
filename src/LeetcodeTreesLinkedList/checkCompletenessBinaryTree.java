e15edf32f35d9386a10b303d7efd28d2ebf5751e
avinash273 <avinash.shanker@mavs.uta.edu>
2020-11-29 21:21:17 -0600

    checkCompletenessBinaryTree

--- src/LeetcodeTreesLinkedList/checkCompletenessBinaryTree.java
+++ src/LeetcodeTreesLinkedList/checkCompletenessBinaryTree.java
@@ -16,9 +16,6 @@ public class checkCompletenessBinaryTree {
         TreeNode left;
         TreeNode right;
 
-        TreeNode() {
-        }
-
         TreeNode(int val) {
             this.val = val;
         }
