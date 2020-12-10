3725e2a7e5e95600549bf6175410e5d9c3888b71
avinash273 <avinash.shanker@mavs.uta.edu>
2020-12-09 21:38:00 -0600

    maxarea

--- src/prep/maxArea.java
+++ src/prep/maxArea.java
@@ -30,10 +30,6 @@ public class maxArea {
                 if (height[left] > height[right]) {
                     maxArea = Math.max(maxArea, height[right] * (right - left));
                     right--;
-                } else {
-                    maxArea = Math.max(maxArea, height[left] * (right - left));
-                    left++;
-                }
             }
             return maxArea;
         }
