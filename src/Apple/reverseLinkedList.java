2d227237bb263ede9841824b002adc82d852098a
avinash273 <avinash.shanker@mavs.uta.edu>
2021-01-13 00:47:27 -0600

    reverseLinkedList

---
+++ src/Apple/reverseLinkedList.java
@@ -0,0 +1,22 @@
+package Apple;
+
+public class reverseLinkedList {
+    class ListNode {
+        int val;
+        ListNode next;
+    }
+
+    class Solution {
+        public ListNode reverseList(ListNode curr) {
+            ListNode prev = null;
+
+            while (curr != null) {
+                ListNode currNext = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = currNext;
+            }
+            return prev;
+        }
+    }
+}
