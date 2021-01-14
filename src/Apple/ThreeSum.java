765b1304b95521aad55c4d5aa66a6ed6d3efccba
avinash273 <avinash.shanker@mavs.uta.edu>
2021-01-13 20:37:14 -0600

    ThreeSum

---
+++ src/Apple/ThreeSum.java
@@ -0,0 +1,20 @@
+package Apple;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum {
+    class Solution {
+        public List<List<Integer>> threeSum(int[] nums) {
+            Arrays.sort(nums);
+            List<List<Integer>> res = new ArrayList<>();
+            for (int i = 0; i < nums.length && nums[i] <= 0; i++) {
+                if (i == 0 || nums[i - 1] != nums[i]) {
+//                    threeSum(nums, i, res);
+                }
+            }
+            return res;
+        }
+    }
+}
