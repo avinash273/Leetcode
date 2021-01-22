478f4324e52ad88b0e091d4b208ae30042c26a9b
avinash273 <avinash.shanker@mavs.uta.edu>
2021-01-21 23:43:51 -0600

    ThreeSum

--- src/Apple/ThreeSum.java
+++ src/Apple/ThreeSum.java
@@ -17,17 +17,4 @@ public class ThreeSum {
             return res;
         }
     }
-    class Solutio2n {
-        public List<List<Integer>> threeSum(int[] nums) {
-            Arrays.sort(nums);
-            List<List<Integer>> res = new ArrayList<>();
-            for (int i = 0; i < nums.length && nums[i] <= 0; i++) {
-                if (i == 0 || nums[i - 1] != nums[i]) {
-                    i = i+1;
-//                    threeSum(nums, i, res);
-                }
-            }
-            return res;
-        }
-    }
 }
