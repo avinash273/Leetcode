6761068e549db7137952f22fc7067df09df9f86b
avinash273 <savinash273@gmail.com>
2024-06-23 00:35:02 -0500

    isAnagram242

--- src/main/java/org/code/App.java
+++ src/main/java/org/code/App.java
@@ -1,13 +1,17 @@
 package org.code;
+import org.code.Leetcode2024.isAnagram242;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        /**
+         * isAnagram
+         */
+        isAnagram242 checkAnagram = new isAnagram242();
+        System.out.println(checkAnagram.isAnagramSol1("anagram", "nagaram"));
+        System.out.println(checkAnagram.isAnagramSol2("anagram", "nagaram"));
     }
 }
---
+++ src/main/java/org/code/Leetcode2024/isAnagram242.java
@@ -0,0 +1,35 @@
+package org.code.Leetcode2024;
+
+import java.util.HashMap;
+
+public class isAnagram242 {
+    public boolean isAnagramSol1(String s, String t) {
+        if(s.length() != t.length()) return false;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        System.out.println(s.chars());
+
+        s.chars().forEach(ch -> map.put(ch, (map.getOrDefault(ch, 0)) + 1));
+        t.chars().forEach(ch -> map.put(ch, (map.getOrDefault(ch, 0)) - 1));
+
+        return map.values().stream().allMatch(v -> v == 0);
+    }
+
+    public boolean isAnagramSol2(String s, String t) {
+        int sLen = s.length();
+        int tLen = t.length();
+        int[] charMap = new int[26];
+
+        if(sLen != tLen) return false;
+
+        for(int i = 0; i < sLen; i++){
+            charMap[s.charAt(i) - 'a']++;
+        }
+
+        for(int i = 0; i < tLen; i++){
+            charMap[t.charAt(i) - 'a']--;
+            if(charMap[t.charAt(i)  - 'a'] < 0) return false;
+        }
+        return true;
+    }
+}
