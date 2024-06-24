c568994bc2fb296d0b61a95f84143b79413a7127
avinash273 <savinash273@gmail.com>
2024-06-23 20:11:06 -0500

    isPowerOfTwoConstantTime

---
+++ src/main/java/org/code/Leetcode2024/CheckIsPowerOfTwo.java
@@ -0,0 +1,21 @@
+package org.code.Leetcode2024;
+
+public class CheckIsPowerOfTwo {
+    public boolean isPowerOfTwoLogN(int n) {
+        long power = 1;
+        while(power < n){
+            power *= 2;
+        }
+        return (power == n);
+    }
+    public boolean isPowerOfTwoConstantTime(int n) {
+        /**The idea behind binary of power of 2 is all 0 bits with just 1 one bit.
+         x & (-x) == x then power of 2
+         -x = !x + 1
+         */
+        long x = n;
+        if(n == 0)return false;
+        return (x & (-x)) == x;
+
+    }
+}
