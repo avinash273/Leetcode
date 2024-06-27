46932cfd6c3408b8ade21c8cf8104c07c360b3c7
avinash273 <savinash273@gmail.com>
2024-06-27 15:03:19 -0500

    RemoveElement27

--- src/main/java/org/code/Leetcode2024/RemoveElement27.java
+++ src/main/java/org/code/Leetcode2024/RemoveElement27.java
@@ -3,7 +3,7 @@ package org.code.Leetcode2024;
 public class RemoveElement27 {
     public int removeElement(int[] nums, int val) {
         int left = 0;
-        int right = nums.length - 1;
+        int right = nums.length - 1;;
 
         while(left <= right){
             if(nums[left] == val){
