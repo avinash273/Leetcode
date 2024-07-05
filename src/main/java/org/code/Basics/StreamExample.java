73f46ee0deefc96fe5232943d04db323cbdc071f
avinash273 <savinash273@gmail.com>
2024-07-04 21:22:20 -0500

    Map reduce example

--- src/main/java/org/code/Basics/StreamExample.java
+++ src/main/java/org/code/Basics/StreamExample.java
@@ -8,8 +8,17 @@ import java.util.stream.Collectors;
 public class StreamExample {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-        List<Integer> list1 = list.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
+        List<Integer> list1 = list.stream().filter(x -> x % 2 == 0).map(x -> x * 2).collect(Collectors.toList());
+        int list3 = list.stream().filter(x -> x % 2 == 0).map(x -> x * 2).reduce(0, (c,e) -> c + e);
 
-        list.forEach(x -> System.out.print(x + " "));
+        list1.forEach(x -> System.out.print(x + " "));
+        System.out.println("list 3" + list3);
+
+        //Map reduce example
+        List<String> letters = Arrays.asList("a", "b", "c", "d", "e", "f");
+        String answer = letters.stream().map(x -> x.toUpperCase()).reduce("",(a, b) -> a + b);
+        String name = letters.stream().reduce("", (a,b) -> a + b + "hello");
+        System.out.println(answer);
+
     }
 }
