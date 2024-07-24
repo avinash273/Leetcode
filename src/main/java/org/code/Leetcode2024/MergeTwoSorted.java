da5450142f4960c4e214d8bc0b822b913af70e86
avinash273 <savinash273@gmail.com>
2024-07-23 23:51:53 -0500

    MergeTwoSorted

---
+++ src/main/java/org/code/Leetcode2024/MergeTwoSorted.java
@@ -0,0 +1,16 @@
+package org.code.Leetcode2024;
+
+class MergeTwoSorted {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int p1 = m - 1;
+        int p2 = n - 1;
+
+        for(int p = m + n - 1; p >= 0; p--){
+            if(p2 < 0) break;
+
+            if(p1 >=0 && nums1[p1] > nums2[p2]) nums1[p] = nums1[p1--];
+            else nums1[p] = nums2[p2--];
+        }
+    }
+}
